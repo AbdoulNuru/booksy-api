1fbe135b4fbe2bb80fc08f517ddab68e0892b893
Abdoul <abdoulniyigena@gmail.com>
2020-04-07 10:55:48 +0200

    user authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Models from "../database/models";
 import toAuthJSON from "../helpers/toAuthJSON";
+import { comparePassword } from "../helpers/jwt";
 
 const { Users } = Models;
 const router = express.Router();
@@ -10,8 +11,12 @@ router.post("/", async (req, res) => {
   const user = await Users.findOne({
     where: { email: credentials.email },
   });
+  const truePassword = await comparePassword(
+    credentials.password,
+    user.password
+  );
 
-  if (user && user.email === credentials.email) {
+  if (user && truePassword) {
     res.json({ user: toAuthJSON(user.email) });
   } else {
     res.status(404).json({ errors: { global: "Invalid credentials" } });
