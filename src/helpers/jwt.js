8313b26a3a07ecd0aa3e1a0adfa2b00c6b8eb3be
Abdoul <abdoulniyigena@gmail.com>
2020-04-07 10:55:48 +0200

    user authentication

--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -21,4 +22,25 @@ export const encode = (email) => {
 export const decode = (token) => {
   const payload = jwt.verify(token, process.env.JWT_SECRET);
   return payload;
+};
+
+/**
+ * @description decoding token method
+ * @param {string} password
+ * @returns {string} hash
+ */
+export const hashPassword = (password) => {
+  const hash = bcrypt.hashSync(password, 10);
+  return hash;
+};
+
+/**
+ * @description decoding token method
+ * @param {string} password
+ * @param {string} hash
+ * @returns {boolean} compare
+ */
+export const comparePassword = (password, hash) => {
+  const compare = bcrypt.compareSync(password, hash);
+  return compare;
 };
