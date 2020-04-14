f5bee1c4f10d56e704dd46388b588eef0b782253
Abdoul <abdoulniyigena@gmail.com>
2020-04-14 11:11:35 +0200

    fix the routing error

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,25 +1,46 @@
 import express from "express";
 import uuid from "uuid/v4";
 import Models from "../database/models";
-import { hashPassword } from "../helpers/jwtHelpers";
+import {
+  hashPassword
+} from "../helpers/jwtHelpers";
 import toAuthJSON from "../helpers/toAuthJSON";
 
-const { Users } = Models;
+const {
+  Users
+} = Models;
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { email, password } = req.body.user;
+  const {
+    email,
+    password
+  } = req.body.user;
   const hash = hashPassword(password);
-  const userExist = await Users.findOne({ where: { email } });
+  const userExist = await Users.findOne({
+    where: {
+      email
+    }
+  });
 
   if (userExist) {
     return res
       .status(400)
-      .json({ errors: { global: "The user account already exist" } });
+      .json({
+        errors: {
+          global: "The user account already exist"
+        }
+      });
   }
 
-  const save = Users.create({ id: uuid(), email, password: hash });
-  res.status(201).json({ user: toAuthJSON(email, save.isConfirmed) });
+  const save = Users.create({
+    id: uuid(),
+    email,
+    password: hash
+  });
+  res.status(201).json({
+    user: toAuthJSON(email, save.isConfirmed)
+  });
 });
 
 export default router;
